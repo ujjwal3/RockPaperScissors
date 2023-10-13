d7b313f0d0bd0806fcaf80a541556eb4c87eb54b
Ujjwal <42678242+ujjwal3@users.noreply.github.com>
2023-10-13 12:53:13 +0530

    Add files via upload

---
+++ RockPaperScissors.py
@@ -0,0 +1,35 @@
+import random
+import time
+
+print("Starting Game",end='')
+
+print("Welcome To Rock Paper Seissor Game")
+print("1. Rock")
+print("2. Paper")
+print("3. Scissors")
+
+
+x =random.randint(1,3)
+
+
+y = int(input("Enter a number from given index: "))
+if y>3:
+    print("Number is not present")
+    print("Exitting!")
+    exit()
+elif x == y:
+    print("Draw")
+    print(f"Computer choosed {x}")
+elif x == 1 and y == 3:
+    print("Sorry! You Lost") 
+    print("Computer Choosed Rock")  
+elif x == 2 and y == 1:
+    print("Sorry! You Lost")
+    print("Computer Choosed Paper")
+elif x == 3 and y == 2:
+    print("Sorry! You Lost")
+    print("Computer Choosed Scissors")
+
+else:
+    print("Congratulations! You Win")
+
